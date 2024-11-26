ea65f1739df3b13ea13eeea6cde307540b36916e
Uczeń <Ucze@DESKTOP-LR2HC22>
2024-11-26 14:37:49 +0100

    Dodaj pliki projektów.

---
+++ kosci/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace kosci
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ kosci/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace kosci
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
